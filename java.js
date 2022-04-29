aa7b29ed32724bf7ce75ab32223af1b28d75f163
kahkashan369 <kahkashanirfan369@gmail.com>
2022-04-29 19:39:11 +0500

    javascript

---
+++ java.js
@@ -0,0 +1,843 @@
+    
+                                //         Chapter # 1 (Alerts)
+
+                                //   *******************************
+                                // ***********************************
+
+
+
+
+  //   Question No : 1
+  // *******************    
+  
+  
+
+// alert ("Wellcome user");
+
+
+
+
+
+  
+  //   Question No : 2
+  // *******************                               
+
+
+
+//  alert ("Error! please enter a valid password.");
+ 
+
+
+
+
+
+  
+  //   Question No : 3
+  // *******************                               
+
+
+
+// alert ("Welcome to JS Land... \n Happy Coding!");
+
+
+
+
+
+
+  
+  //   Question No : 4
+  // *******************                               
+
+
+
+// alert ("Welcome to JS Land...");
+
+
+// alert ("Happy Coding! \n Prevent this page creating additional dailogs.");
+
+
+
+  
+   
+
+
+
+
+  //   Question No : 5
+  // *******************                               
+
+
+
+
+
+// alert ("Hello... I can run JS throught my web browser's console");
+
+
+// console.log (alert);
+
+
+
+
+
+
+  
+  //   Question No : 6 & 7
+  // **********************                               
+ 
+
+// (***Note*** "Please check HTML file")
+
+
+
+
+
+
+  
+
+                          //       Chapter # 2 ( Variables for Strings )
+
+                          //  ********************************************
+                          //       ***********************************
+
+
+
+
+
+
+
+//   Question No : 1
+// *******************    
+                                
+                                
+                              
+// var userName ;
+
+
+
+
+
+
+
+// Question No : 2
+// *******************    
+
+
+// var myName = "Kahkashan Irfan";
+
+
+
+
+
+
+
+
+
+// Question No : 3
+// *******************    
+
+
+
+
+// var titledMessage = "Hell World";
+ 
+// alert(titledMessage);
+
+
+
+
+
+
+
+
+
+// Question No : 4
+// *******************    
+
+
+
+
+// var studentName = "Johne Doe";
+
+// var age = "15";
+
+// var qualification = "certified web & mobile app"
+
+
+// alert(studentName);
+
+// alert(age + "year old");
+
+// alert(qualification);
+
+
+
+
+
+
+
+
+// Question No : 5
+// *******************    
+
+
+
+// var pizza = "Pizza \n Pizz \n Piz \n Pi \n P" ;
+
+
+// alert(pizza);
+
+
+
+
+
+
+
+
+
+// Question No : 6
+// *******************    
+
+
+
+
+// var email = "kahkashanirfan369@gmail.com"
+
+// alert( "My email address is "  +   email);
+
+
+
+
+
+
+
+
+// Question No : 7
+// *******************    
+
+
+
+// var book = "A smarter way to learn in javascript";
+
+// alert(" I am trying to learn from the book... \n A smarter way to learn in Javascript");
+
+
+
+
+
+
+
+
+
+
+
+//   Question No : 8
+// *******************    
+
+
+
+
+
+// var content = "yeah! I can write HTML Content throught JavaScript";
+
+// document.write(content);
+
+
+
+
+
+
+
+
+
+//   Question No : 9
+// *******************    
+
+
+
+
+// var specialDesign = " “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” " ;
+
+
+// alert(specialDesign);
+
+
+// document.write(specialDesign);
+
+
+
+
+
+
+
+
+
+
+                          //     Chapter # 3 ( Variables for Number )
+
+                          // ********************************************
+                          //      ***********************************
+
+
+
+
+
+
+  
+//    Question No : 1
+// ********************    
+                               
+
+
+
+// var age = "I am 24 year old";
+
+// alert(age);
+
+
+
+
+  
+
+
+
+
+
+//    Question No : 2
+// ********************    
+                               
+            
+
+// var visitedTime =prompt( "How many time you visited this site ");
+
+// alert("You have visited this site " + visitedTime + " times ");
+
+
+
+
+
+  
+
+
+
+//    Question No : 3 
+// ********************    
+                               
+               
+
+
+
+// var birthYear = "My date of birth is 1998 </br> data type of my decleared variable is number";
+
+// document.write(birthYear);
+
+
+
+
+
+
+
+
+  
+//    Question No : 4
+// ********************    
+                               
+         
+
+
+// var visitorName = prompt("Enter your Name");
+
+// var productTitle = prompt("Please enter your product title ");
+ 
+// var quantity = prompt("How many products you want to order");
+                               
+                               
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store."); 
+
+
+
+
+
+
+
+                                        
+                              //    Chapter # 4( Variables Names : legal & illegal )
+
+                              // ********************************************
+                              //      ***********************************
+    
+    
+
+
+ 
+//     Question No : 1
+//  ********************     
+                 
+
+
+// var num1 = 2;
+// var num2 = 4;
+// var num3 = 6;
+
+// var totalNum = num1 + num2 + num3;
+
+//  document.write(totalNum);
+
+
+
+
+
+//    Question No : 2
+//  ********************    
+           
+
+
+//****/ legal variables****
+
+
+// var user = "Kahkashan";
+
+// var userName = "Zartasha"
+
+// var $name ="Aizal";
+
+// var _user = "Fatima";
+
+// var user123 = "Aisha";
+
+
+
+
+
+
+
+//****/ Illegal variables****
+
+
+
+// var User = "Kahkashan";
+
+// var username = "Zartasha"
+
+// var #name ="Aizal";
+
+// var -user = "Fatima";
+
+// var 123user = "Aisha";
+
+
+
+
+
+
+
+//    Question No : 3
+//  ********************    
+          
+
+
+
+// document.write("<h1>Rules for naming JS variable </h1></br>  A variable name can't contain any spaces.</br> A variable name can contain only letters, numbers, dollar signs, and underscores.</br>variable name are case sensitive</br>variable name should not be JS keyword")
+
+
+
+
+
+
+
+                                        
+                            //       Chapter # 5 ( Math Expression )
+
+                            // ********************************************
+                            //      ***********************************
+
+
+
+                                 
+
+//    Question No : 1
+// ********************    
+                                           
+                      
+
+// var num1 = 3 
+// var num2 = 5
+// var totalNum = num1 + num2;
+// document.write( "Sum of 3 & 5 is " + "=" + totalNum );
+
+
+
+
+
+
+                            
+
+//   Question No : 2
+// ********************    
+                                           
+                
+
+
+
+// var num1 = 5 
+// var num2 = 3
+// var totalNum = num1 - num2;
+// document.write( "Subtraction of 3 & 5 is " +  " = " +  totalNum );
+
+
+
+                
+
+// var num1 = 5 
+// var num2 = 3
+// var totalNum = num1 * num2;
+// document.write( "Multiplication of 3 & 5 is " +  " = " +  totalNum );
+
+
+
+
+// var num1 = 5 
+// var num2 = 3
+// var totalNum = num1 / num2;
+// document.write( "Division of 3 & 5 is " +  " = " +  totalNum );
+
+
+
+// var num1 = 5 
+// var num2 = 3
+// var totalNum = num1 % num2;
+// document.write( "Modules of 3 & 5 is " +  " = " +  totalNum );
+
+
+
+
+
+
+
+                            
+
+//   Question No : 3
+// ********************    
+                                           
+    
+// A) 
+
+//  var userName ;
+
+// b) 
+
+//  var userName = "Kahkashan";
+
+// c) 
+
+//  var initializeNum = 23
+
+
+// d) 
+//   var num = 5;
+//    document.write(num);
+ 
+
+
+//  e)
+
+//  var i = 2;
+//  var j = i+1
+
+// document.write(j);
+
+
+// f)
+
+// var num = 5;
+// var incrementNum = num + 1;
+
+// document.write(incrementNum);
+
+
+// g)
+
+// var num = 4;
+// var totalNum = num + 3;
+
+// document.write(totalNum);
+
+
+// h)
+
+// var num = 10;
+// var incrementNum = num + 3;
+
+// document.write(incrementNum);
+
+
+// i)
+ 
+// var num = 15;
+// var decrementValue = num - 5; 
+
+// document.write(decrementValue);
+
+// j)
+
+
+// var num = 30;
+// var decrementValue = num - 18;
+
+// document.write(decrementValue);
+
+
+
+// k)
+
+
+// var division = 30;
+// var reminderValue = division / 10;
+ 
+// document.write(reminderValue);
+
+
+// l)
+
+
+// var num = 20;
+// var reminderValue = num % 10;
+ 
+// document.write(reminderValue);
+
+
+                            
+
+
+
+
+
+//   Question No : 4
+// ********************    
+                                           
+                
+
+// var oneMovieTicket = 600;
+// var totalCostOfMovie = oneMovieTicket * 5;
+
+// document.write("Total cost to buy 5 tickets to a movie is" + " " +  totalCostOfMovie + "PKR");
+
+
+
+
+
+                            
+
+//   Question No : 5
+// ********************    
+                                           
+                
+
+// var tableName = 9;
+// document.write("Table of 9" + "</br>" + "</br>")
+
+// document.write ( tableName + ' x ' + 1 + '=' + tableName * 1  + "</br>"); 
+// document.write ( tableName + ' x ' + 2 + '=' + tableName * 2  + "</br>" );
+// document.write ( tableName + ' x ' + 3 + '=' + tableName * 3  + "</br>");
+// document.write ( tableName + ' x ' + 4 + '=' + tableName * 4  + "</br>");
+// document.write ( tableName + ' x ' + 5 + '=' + tableName * 5  + "</br>");
+// document.write ( tableName + ' x ' + 6 + '=' + tableName * 6  + "</br>");
+// document.write ( tableName + ' x ' + 7 + '=' + tableName * 7  + "</br>");
+// document.write ( tableName + ' x ' + 8 + '=' + tableName * 8  + "</br>" );
+// document.write ( tableName + ' x ' + 9 + '=' + tableName * 9  + "</br>");
+// document.write ( tableName + ' x ' + 10 + '=' + tableName * 10  + "</br>" );
+
+
+
+
+
+
+                            
+
+//   Question No : 6
+// ********************    
+                                           
+             
+
+
+// var  celsius = 25;
+// var fahrenheit = 70;
+
+
+// var celsiusTem = (fahrenheit - 32) * 5 / 9;
+// document.write("70F is " + celsiusTem);
+
+
+
+// var farenheitTem = (celsius * 9 / 5) + 32 ;
+// document.write("25C is " + farenheitTem);
+
+
+
+
+
+
+                            
+
+//   Question No : 7
+// ********************    
+                                           
+                
+
+// var priceOfItem1 = 650 ;
+
+// document.write("Price of item 1 is" +  " = " + priceOfItem1 + "</br>");
+
+// var orderQuantityOfItem1 = 3 ;
+ 
+// document.write("Quantity of item 1 is" + " = " + 3 + "</br>");
+
+// var totalCostOfItem1 = priceOfItem1 * orderQuantityOfItem1;
+ 
+// document.write("Total cost of item 1" + " = "  + totalCostOfItem1 + "  " + "</br>" + "</br>");
+
+// var priceOfItem2 = 100;
+
+// document.write("Price of item 2 is" + " = "  + priceOfItem2 + "</br>");
+
+// var orderQuantityOfItem2 = 7 ;
+
+// document.write("Quantity of item 2 is" + " = " + 7 + "</br>");
+
+// var totalCostOfItem2 = priceOfItem2 * orderQuantityOfItem2;
+
+// document.write("Total cost of item 2" + " = "  + totalCostOfItem2 + "  " + "</br>" + "</br>" );
+
+// var shippingCharges = 100 ;
+
+// document.write("Shipping charges" + " = "  + shippingCharges + "  " + "</br>" + "</br>");
+
+// var totalCostOfOrder = totalCostOfItem1 + totalCostOfItem2 + shippingCharges;
+
+// document.write("Total cost of your order is" +  " = "  + totalCostOfOrder);
+
+
+
+
+                            
+
+//   Question No : 8
+// ********************    
+                                           
+              
+
+// var totalMarks = 980;
+// document.write("Total marks: " + totalMarks + "</br>");
+// var MarksObtained = 804;
+// document.write("Marks obtained: " + MarksObtained + "</br>");
+// var percentageOfMarks = MarksObtained * 100 / totalMarks; 
+ 
+// document.write( "Percentage: " + percentageOfMarks + " % ");
+
+
+
+
+
+
+
+                            
+
+//   Question No : 9
+// ********************    
+                                           
+                
+ 
+// var usDollar =10;
+// var usDollarConvertRupeee = 104.80;
+// var dollarConvertInPakistaniRupees = usDollar * usDollarConvertRupeee;
+
+
+
+// var saudiRiyal = 25;
+// var saudiRiyalConvertRupee = 28;
+// var saudiRiyalConvertInpakistaniRupees = saudiRiyal * saudiRiyalConvertRupee;
+
+// var totalcurrencyInPkr = dollarConvertInPakistaniRupees + saudiRiyalConvertInpakistaniRupees;
+
+// document.write("Total currency in PKR: " + totalcurrencyInPkr);
+
+
+
+                            
+
+//   Question No : 10
+// ********************    
+             
+
+
+                
+// var num = 10;
+// var totalNUm = num + 5 * 10 / 2
+// document.write(totalNUm);
+
+
+
+                            
+
+//   Question No : 11
+// ********************    
+                                           
+                
+
+// var currentYear = 2016;
+
+// var birthYear = 1992;
+ 
+// var yourAge = currentYear - birthYear;
+
+// document.write(yourAge);
+
+
+
+
+
+
+
+                            
+
+//   Question No : 12
+// ********************    
+                                           
+  
+
+
+// var radius = 20;
+
+// document.write("Radius of a circle: " + radius + "</br>");
+
+// var piValue = 3.142;
+
+// var circumferenceValue = 2 * piValue *radius ; 
+
+// document.write("The circumference is: "  +  circumferenceValue + "</br>" ); 
+
+// var areaOfcircle = piValue * radius * radius;
+
+// document.write("The area is: " + areaOfcircle);
+
+
+
+                            
+
+//   Question No : 13
+// ********************    
+                                           
+                
+
+// var favSnack = "choclate chip";
+
+// var currentAge = 15;
+
+// var estimatedMaxAge = 65;
+
+// var amountOfSnacksPerDay = 3;
+
+// var age =estimatedMaxAge - currentAge;
+
+// var totalsnacks =  age * amountOfSnacksPerDay;
+
+// document.write("You will need " + totalsnacks + "  " + "choclate chip to last you untill the rip old age of 65 ");
